4f3a4a843357915452575592966fb45830cca963
abdellah711 <alaouiabdellah711@gmail.com>
2022-05-22 15:31:12 +0100

    save preferred theme in localstorage

--- context/switch-theme.tsx
+++ context/switch-theme.tsx
@@ -1,4 +1,4 @@
-import { useContext, createContext, useState, FC, ReactNode } from "react";
+import { useContext, createContext, useState, FC, ReactNode, useEffect } from "react";
 import { ThemeProvider } from "styled-components";
 import { DarkTheme, LightTheme } from "styles/theme.styled";
 
@@ -12,8 +12,20 @@ export const useSwitchTheme = () => {
 export const SwitchThemeProvider: FC<{ children?: ReactNode }> = ({ children }) => {
     const [theme, setTheme] = useState(LightTheme)
 
+    useEffect(() => {
+        let isDarkTheme: string | null | boolean = localStorage.getItem('dark-theme')
+        if (!isDarkTheme) {
+            isDarkTheme = matchMedia('(prefers-color-scheme: dark)').matches
+        } else {
+            isDarkTheme = JSON.parse(isDarkTheme)
+        }
+        setTheme(isDarkTheme ? DarkTheme : LightTheme)
+    }, [])
 
-    const switchTheme = () => setTheme(theme === LightTheme ? DarkTheme : LightTheme)
+    const switchTheme = () => {
+        localStorage.setItem('dark-theme',JSON.stringify(theme === LightTheme))
+        setTheme(theme === LightTheme ? DarkTheme : LightTheme)
+    }
     const isDark = theme === DarkTheme
 
     return (
