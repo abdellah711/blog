e449d32222951284a4cdd6529935fd81be11bb72
abdellah711 <alaouiabdellah711@gmail.com>
2022-06-09 19:19:15 +0100

    fix .env typo + update README.md

--- config/variables.ts
+++ config/variables.ts
@@ -1,4 +1,4 @@
 export const GRAPHQL_URL = <string>process.env.NEXT_APP_GRAPHQL_URL
 export const GRAPHQL_TOKEN = <string>process.env.NEXT_APP_GRAPHQL_TOKEN
 export const PAGE_ARTICLES_COUNT = +process.env.NEXT_PUBLIC_PAGE_ARTICLES_COUNT!
-export const CATEGORIES_SIDEBAR_COUNT = +process.env.NEXT_PUBLIC_PAGE_ARTICLES_COUNT!
+export const CATEGORIES_SIDEBAR_COUNT = +process.env.NEXT_PUBLIC_CATEGORIES_SIDEBAR_COUNT!
--- pages/categories.tsx
+++ pages/categories.tsx
@@ -1,7 +1,7 @@
 import CategoriesList from 'components/Categories'
 import Layout from 'components/Layout'
 import RecentPosts from 'components/Sidebar/RecentPosts'
-import { GetStaticProps, NextPage } from 'next'
+import { GetServerSideProps, NextPage } from 'next'
 import { getAllCategories } from 'services/categories'
 import { IArticle } from 'types/article'
 import { ICategorie } from 'types/categorie'
@@ -25,7 +25,7 @@ interface CategoriesPageProps {
     recentArticles: IArticle[];
 }
 
-export const getStaticProps: GetStaticProps<CategoriesPageProps> = async () => {
+export const getServerSideProps: GetServerSideProps<CategoriesPageProps> = async () => {
     const { categories, recent } = await getAllCategories()
     return {
         props: {
